a0e4af27be5102329b39a465d6b996e9a795a45b
Damir Mehic <damir.mehic@edu.varberg.se>
2023-10-09 10:58:33 +0200

    added models

---
+++ BusinessPortal/Models/Personal.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessPortal.Models
+{
+    public class Personal
+    {
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ BusinessPortal/Models/RequestType.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessPortal.Models
+{
+    public class RequestType
+    {
+        public int Id { get; set; }
+        public string Vabb { get; set; }
+        public string Sick { get; set; }
+        public string Vacation { get; set; }
+    }
+}
