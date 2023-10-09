cd9a1560ad34391e58acbfb8a5ca979d882d2e29
Damir Mehic <damir.mehic@edu.varberg.se>
2023-10-09 12:03:01 +0200

    added data to Repo

--- BusinessPortal/IRepository/PersonalRepository.cs
+++ BusinessPortal/IRepository/PersonalRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace BusinessPortal.IRepository
+﻿using BusinessPortal.Data;
+using BusinessPortal.Models;
+
+namespace BusinessPortal.IRepository
 {
-    public class PersonalRepository
+    public class PersonalRepository : IRepository<Personal>
     {
+        private readonly BusinessContext _db;
+
+        public PersonalRepository(BusinessContext db)
+        {
+            this._db = db;
+        }
+        public Task Create(Personal t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Personal t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Personal>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Personal> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Personal t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- BusinessPortal/IRepository/RequestRepository.cs
+++ BusinessPortal/IRepository/RequestRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace BusinessPortal.IRepository
+﻿using BusinessPortal.Data;
+using BusinessPortal.Models;
+
+namespace BusinessPortal.IRepository
 {
-    public class RequestRepository
+    public class RequestRepository : IRepository<Request>
     {
+
+        private readonly BusinessContext _db;
+
+        public RequestRepository(BusinessContext db)
+        {
+            this._db = db;
+        }
+        public Task Create(Request t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Request t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Request>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Request> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Request t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
