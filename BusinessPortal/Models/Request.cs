da2210034573b6761c43900f8f0c5193f6ec6d54
Filip Mossberg <filip.mossberg@gmail.com>
2023-10-09 11:11:30 +0200

    Added Request Model

---
+++ BusinessPortal/Models/Request.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessPortal.Models
+{
+    public class Request
+    {
+        [Key]
+        public int Id { get; set; }
+        public int RequestTypeId { get; set; }
+        public DateTime Period { get; set; }
+        public DateTime RequestDate { get; set; }
+        public int PersonalId { get; set; }
+        public bool Status { get; set; }
+    }
+}
